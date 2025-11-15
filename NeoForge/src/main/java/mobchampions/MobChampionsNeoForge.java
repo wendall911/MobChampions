247d96712969c89aa473c42361318f6b2d62d4d6
Wendall Cada <wendallc@83864.com>
2025-11-15 12:17:03 -0800

    Defer config setup until all registries are loaded.

--- NeoForge/src/main/java/mobchampions/MobChampionsNeoForge.java
+++ NeoForge/src/main/java/mobchampions/MobChampionsNeoForge.java
@@ -10,12 +10,15 @@ import net.minecraft.resources.ResourceLocation;
 
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.network.event.RegisterPayloadHandlersEvent;
 import net.neoforged.neoforge.network.registration.PayloadRegistrar;
 import net.neoforged.neoforge.registries.RegisterEvent;
 
 import mobchampions.attachments.AttachmentsRegistry;
 import mobchampions.common.effect.MobChampionsEffects;
+import mobchampions.event.ServerEventListener;
 import mobchampions.network.MobChampionsNeoForgeNetwork;
 import mobchampions.network.LaunchFireworksPacket;
 import mobchampions.network.SyncMobChampionData;
@@ -29,6 +32,7 @@ public class MobChampionsNeoForge {
         AttachmentsRegistry.init(eventBus);
         MobChampions.initConfig();
         eventBus.addListener(this::registerPayloadHandler);
+        eventBus.addListener(this::setup);
     }
 
     private void registerPayloadHandler(final RegisterPayloadHandlersEvent event) {
@@ -38,6 +42,10 @@ public class MobChampionsNeoForge {
             MobChampionsNeoForgeNetwork.getInstance()::handleFireworksPacket);
         registrar.playToClient(SyncMobChampionData.TYPE, SyncMobChampionData.STREAM_CODEC,
             MobChampionsNeoForgeNetwork.getInstance()::processMobChampionData);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+        NeoForge.EVENT_BUS.register(ServerEventListener.class);
     }
 
     private void registryInit(IEventBus bus) {
